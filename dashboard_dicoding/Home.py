2f582f6f51f2c03ef6634e80659cdbe0efec048e
Yora <91246717+yorsyd@users.noreply.github.com>
2024-10-25 08:23:30 +0700

    Add files via upload

---
+++ dashboard_dicoding/Home.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+def crt_weather_counts(df):
+    weather_counts = df.groupby(by='weathersit').count()
+    return weather_counts
+
+def crt_partOfDay_counts(df):
+    partOfDay_counts = df.groupby(by='part_of_day').count()
+    return partOfDay_counts
+
+def crt_monthly_counts_avg(df):
+    monthly_counts_avg = df.groupby("month")[["casual", "registered"]].mean()
+    return monthly_counts_avg
+
+day_df = pd.read_csv("day_df.csv")
+hour_df = pd.read_csv("hour_df.csv")
+
+min_month = day_df["month"].min()
+max_month = day_df["month"].max()
+
+monthly_counts_avg = crt_monthly_counts_avg(day_df)
+partOfDay_counts = crt_partOfDay_counts(hour_df)
+weather_counts = crt_weather_counts(hour_df)
+
+# with st.sidebar:
+#     st.image(
+#         "https://back.3blmedia.com/sites/default/files/styles/ratio_3_2/public/triplepundit/wide/BlueBikes%20in%20Boston%20is%20among%20many%20successful%20bike%20share%20programs.png?h=85ee54f6",
+#         use_column_width="always"
+#     )
+#     st.write("""
+# Dashboard ini adalah bagian dari rangkaian tugas proyek akhir analisis data yang diadakan oleh dicoding.com 
+# """) 
+
+st.title("Dashboard Analisis Data Bike Sharing")
+st.page_link("https://www.linkedin.com/in/yogarusydia/", label="by: Yoga Rusydi Arifin")
+st.image(
+        "https://back.3blmedia.com/sites/default/files/styles/ratio_3_2/public/triplepundit/wide/BlueBikes%20in%20Boston%20is%20among%20many%20successful%20bike%20share%20programs.png?h=85ee54f6",
+        use_column_width="always"
+    )
+
+st.subheader("Dicuaca Seperti Apa Sepeda Paling Banyak Disewa?")
+#weather_counts
+st.bar_chart(
+    weather_counts['count'], 
+    x_label='Kondisi Cuaca', 
+    y_label='Jumlah Pengguna', 
+    )
+st.caption("""Keterangan:
+           
+1: Clear, Few clouds, Partly cloudy, Partly cloudy | 2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
+ | 3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds
+ | 4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
+""")
+st.write("""Sepeda lebih banyak disewa pada cuaca kategori 1 yaitu sebanyak 11413 pengguna. Sedangkan pada cuaca
+kategori 4 menjadi cuaca dengan pengguna paling sedikit, dengan total hanya 3 pengguna.""")
+
+
+st.subheader("Kapan Sepeda Paling Sering Disewa? Apakah Siang atau Malam?")
+#partOfDay_counts
+st.bar_chart(
+    partOfDay_counts['count'],
+    y_label="Jumlah Pengguna",
+    x_label="Bagian Hari"
+)
+st.write("""Pada siang hari, sepeda paling banyak disewa dengan jumlah 8738 pengguna. 
+Sedangkan pada malam hari, terdapat 8641 pengguna sepeda. Kemudian dari visualisasi
+tersebut, dapat dilihat bahwa perbedaan antara banyak pengguna pada malam hari
+dan siang hari tidak terlalu signifikan.""")
+
+st.subheader("Bagaimana Perbandingan Pengguna Kasual dengan Registered pada Setiap Bulan?")
+#monthly_counts_avg
+st.bar_chart(
+    monthly_counts_avg,
+    x_label='Bulan',
+    y_label='Jumlah Pengguna',
+    stack=False
+)
+st.write("""Pengguna terdaftar terlihat signifikan lebih banyak menyewa sepeda dibanding
+pengguna kasual disetiap bulannya. Berarti, pengguna sepeda kebanyakan adalah pengguna
+yang benar-benar membutuhkan jasa ini karena mereka berlangganan dengan jasa tersebut.""")
+
+st.page_link("pages/Analisis Lanjutan.py", label="Go to Analisis Lanjutan")
---
+++ dashboard_dicoding/pages/Analisis Lanjutan.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+day_df = pd.read_csv("day_df.csv")
+hour_df = pd.read_csv("hour_df.csv")
+
+min_month = day_df["month"].min()
+max_month = day_df["month"].max()
+
+monthly_rentals = day_df.groupby(by='month').sum()
+#monthly_rentals    
+
+st.title("Dashboard Analisis Data Bike Sharing")
+st.page_link("https://www.linkedin.com/in/yogarusydia/", label="by: Yoga Rusydi Arifin")
+st.image(
+        "https://back.3blmedia.com/sites/default/files/styles/ratio_3_2/public/triplepundit/wide/BlueBikes%20in%20Boston%20is%20among%20many%20successful%20bike%20share%20programs.png?h=85ee54f6",
+        use_column_width="always"
+    )
+
+st.subheader("Peforma Jumlah Pelanggan Sepeda Berdasarkan Bulan")
+#partOfDay_counts
+st.markdown("**Peforma Keseluruhan Pelanggan**")
+st.line_chart(
+    monthly_rentals['count'],
+    x_label='Bulan',
+    y_label='Jumlah Pelanggan'
+)
+st.write("""
+Jumlah pelanggan mengalami kenaikan dari bulan ke-1 sampai dengan bulan ke-8. 
+Kemudian mengalami penurunan jumlah pelanggan mulai bulan ke-9 sampai dengan 12
+""")
+
+st.markdown("**Peforma Pelanggan Kasual vs Terdaftar**")
+st.line_chart(
+    monthly_rentals,
+    y = ["registered", "casual"],
+    x_label='Bulan',
+    y_label='Jumlah Pelanggan'
+)
+st.write("""
+Jumlah pelanggan terdaftar (registered) mengalami kenaikan mulai dari bulan ke-1 sampai dengan bulan ke-6. Lalu mengalami penurunan
+pada bulan ke-7 dan menaik lagi di bulan ke-8. Tetapi setelah itu, mengalami penurunan terus menurus sampai bulan ke-12. Selanjutnya, pada
+pelanggan kasual (casual), mengalami kenaikan pelanggan dari bulan ke-1 hingga bulan ke-5. Lalu mengalami sedikit penurunan pada bulan ke-6 dan naik lagi di bulan ke-7.
+Tetapi mengalami penurunan kembali pada bulan ke-8 hingga bulan ke-12
+""")
+
+day_df['year'] = day_df['year'].map({0: 2011, 1: 2012})
+year_rentals = day_df.groupby(by='year').sum()
+
+st.subheader("Perbandingan Jumlah pelanggan Sepeda pada Tahun 2011 dan 2012")
+#partOfDay_counts
+st.bar_chart(
+    year_rentals['count'],
+    x_label='Bulan',
+    y_label='Jumlah pelanggan'
+)
+st.write("""
+Jumlah pelanggan pada tahun 2012 memiliki perbedaan sangat signifikan
+dari jumlah pelanggan pada tahun 2011. Hal ini berarti bisnis mengalami
+peningkatan jumlah pelanggan di tahun kedua
+""")
+
+st.page_link("Home.py",label="Back to Home")
